f38b730fbf59fb6ef63e0b891783443bca7bdf0a
Varad Panch <varadpanch@Varads-MBP.lan>
2022-04-15 13:30:45 -0700

    Changes to Algo.java

--- Algo.java
+++ Algo.java
@@ -4,12 +4,12 @@ public class Algo {
 
     public static void main(String[] args) {
 
-        HashMap<String, Integer> deltaTable = new HashMap<>();
+        HashMap<Character, Integer> deltaTable = new HashMap<>();
         
-        deltaTable.put("A",0);
-        deltaTable.put("C",1);
-        deltaTable.put("G",2);
-        deltaTable.put("T",3);
+        deltaTable.put('A',0);
+        deltaTable.put('C',1);
+        deltaTable.put('G',2);
+        deltaTable.put('T',3);
         
         int deltaArray[][] = new int[][] { { 0, 110, 48, 94 }, { 110, 0, 118, 48 }, { 48, 118, 0, 110 },
                 { 94, 48, 110, 0 } };
